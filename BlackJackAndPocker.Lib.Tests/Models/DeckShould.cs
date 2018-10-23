132c8b8e0ea8b168109b2cf64c6d864c8ba95f13
ANDRES H CARRERA <andreshcar@live.com>
2018-10-23 15:52:23 -0600

    adding more unit tests to make sure the deck is building the way i think it should be.

--- BlackJackAndPocker.Lib.Tests/Models/DeckShould.cs
+++ BlackJackAndPocker.Lib.Tests/Models/DeckShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using BlackJackAndPoker.Enums;
 using BlackJackAndPoker.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,6 +16,42 @@ namespace BlackJackAndPocker.Lib.Tests.Models
             var deck = new Deck();
             //assert 
             Assert.AreEqual(52, deck.Cards.Count());
+        }
+
+        [TestMethod]
+        public void Contain13HeartsAfterNewInstanceCreated()
+        {
+            //arrange / act
+            var deck = new Deck();
+            //assert 
+            Assert.AreEqual(13, deck.Cards.Where((s) => s.Suit == Suit.Heart).Count());
+        }
+
+        [TestMethod]
+        public void Contain13ClubsAfterNewInstanceCreated()
+        {
+            //arrange / act
+            var deck = new Deck();
+            //assert 
+            Assert.AreEqual(13, deck.Cards.Where((s) => s.Suit == Suit.Club).Count());
+        }
+
+        [TestMethod]
+        public void Contain13DiamondsAfterNewInstanceCreated()
+        {
+            //arrange / act
+            var deck = new Deck();
+            //assert 
+            Assert.AreEqual(13, deck.Cards.Where((s) => s.Suit == Suit.Diamond).Count());
+        }
+
+        [TestMethod]
+        public void Contain13SpadesAfterNewInstanceCreated()
+        {
+            //arrange / act
+            var deck = new Deck();
+            //assert 
+            Assert.AreEqual(13, deck.Cards.Where((s) => s.Suit == Suit.Spade).Count());
         }
     }
 }
