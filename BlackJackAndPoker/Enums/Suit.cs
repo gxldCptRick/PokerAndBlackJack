75cc7fe10ad0fd4f4980b3d05ea51698ea31e717
ANDRES H CARRERA <andreshcar@live.com>
2018-10-23 15:30:43 -0600

    adding the first basic form of a card class.

--- BlackJackAndPoker/Enums/Suit.cs
+++ BlackJackAndPoker/Enums/Suit.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlackJackAndPoker
+namespace BlackJackAndPoker.Enums
 {
-    public class Class1
+    public enum Suit
     {
+        Heart,
+        Diamond,
+        Club,
+        Spade
     }
 }
---
+++ BlackJackAndPoker/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using BlackJackAndPoker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackAndPoker.Models
+{
+    public struct Card
+    {
+        public readonly Rank Rank;
+        public readonly Suit Suit;
+        public Card(Rank rank, Suit suit)
+        {
+            this.Rank = rank;
+            this.Suit = suit;
+        }
+    }
+}
