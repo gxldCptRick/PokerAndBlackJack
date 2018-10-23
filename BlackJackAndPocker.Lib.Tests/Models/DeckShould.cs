42c2f4bae33e734186260a1c42cce1cf4f78c7ed
ANDRES H CARRERA <andreshcar@live.com>
2018-10-23 15:47:10 -0600

    renaming the test file from unittest1 to something more expressive such as DeckShould

---
+++ BlackJackAndPocker.Lib.Tests/Models/DeckShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using BlackJackAndPoker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackJackAndPocker.Lib.Tests.Models
+{
+    [TestClass]
+    public class DeckShould
+    {
+        [TestMethod]
+        public void Contain52CardsAfterNewInstanceCreated()
+        {
+            //arrange / act
+            var deck = new Deck();
+            //assert 
+            Assert.AreEqual(52, deck.Cards.Count());
+        }
+    }
+}
