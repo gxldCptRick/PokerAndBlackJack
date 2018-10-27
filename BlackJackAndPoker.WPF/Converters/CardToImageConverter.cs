bbb0309c6b50c7247ce420fa0f1e1ab8408db7f4
ANDRES H CARRERA <andreshcar@live.com>
2018-10-27 01:19:39 -0600

    adding a converter and started work on user control

---
+++ BlackJackAndPoker.WPF/Converters/CardToImageConverter.cs
@@ -0,0 +1,41 @@
+﻿using BlackJackAndPoker.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BlackJackAndPoker.WPF.Converters
+{
+    public class CardToImageConverter : IValueConverter
+    {
+        private static IDictionary<Card, ImageSource> _cardImages;
+        static CardToImageConverter()
+        {
+            _cardImages = new Dictionary<Card, ImageSource>();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource source = null;
+            if (value is Card card)
+            {
+                if (!_cardImages.ContainsKey(card))
+                {
+                    var cRank = (int)card.Rank;
+                    var cSuit = card.Suit.ToString() + "s";
+                    var faceSource = $"../../Assets/Cards/{cSuit}/{cRank}.png";
+                    var image = new ImageSourceConverter().ConvertFromString(faceSource) as ImageSource;
+                    _cardImages[card] = image;
+                }
+                source = _cardImages[card];
+            }
+            return source;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
